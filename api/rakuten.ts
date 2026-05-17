36df8bcfaf73d9bc6c46dc81f9147711baf2c653
shishigashira22-lang <shishigashira22@gmail.com>
2026-05-17 22:47:08 +0900

    Update rakuten.ts

--- api/rakuten.ts
+++ api/rakuten.ts
@@ -6,10 +6,8 @@ const RAKUTEN_AFFILIATE_ID = "0c1b4d96.f8d4f15b.0c1b4d97.6857f611";
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET");
-
   const keyword = (req.query.keyword as string) || "漫画";
   const url = `https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&applicationId=${RAKUTEN_APP_ID}&affiliateId=${RAKUTEN_AFFILIATE_ID}&booksGenreId=001001&keyword=${encodeURIComponent(keyword)}&hits=10&sort=sales`;
-
   try {
     const response = await fetch(url);
     const data = await response.json();
